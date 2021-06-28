3507fbd23487fa285397595134231e720f22e5b2
hetan <hetan123456>
2021-06-28 10:03:34 +0800

    Version Test

---
+++ app/src/main/java/com/app/gitdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.app.gitdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    //1
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
